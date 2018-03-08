1f14f1048d96758c4abddcf42fe48758e483552c
lixi <1595607510@qq.com>
2018-03-08 21:52:23 +0800

    添加第三方库

---
+++ testNoti/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  testNoti
+//
+//  Created by Sumency on 2018/3/8.
+//  Copyright © 2018年 com.Qiaodu. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
